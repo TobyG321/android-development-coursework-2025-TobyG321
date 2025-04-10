ee95281a5a76e7c279e8d73e42709e4edf9aec7d
TobyG321 <22012863@Hope.ac.uk>
2025-04-10 23:08:12 +0100

    Turn the first fragment into a login page

--- app/src/main/java/uk/ac/hope/mcse/android/coursework/FirstFragment.java
+++ app/src/main/java/uk/ac/hope/mcse/android/coursework/FirstFragment.java
@@ -32,7 +32,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(v ->
+        binding.buttonLogin.setOnClickListener(v ->
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment)
         );
